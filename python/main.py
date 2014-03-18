969962d79dafd76288b33d31700b1209d0dad270
palcain <palcain@hp83-inf-9.ictp.it>
2014-03-18 18:55:57 +0100

    A couple of types resolved, throws a SIGSEGV

--- python/main.py
+++ python/main.py
@@ -1,16 +1,35 @@
+#!/usr/bin/python
 from result import Result
+from class_and_funtion import *
+
+md=CDLL("../libljmd-serial.so")
 
 ###--- variables will be comes from program
 ### this import is valid for only data file
+import sys
 import numpy as np
 
-filename = 'argon_108.dat'
+if __name__ == "__main__":
+   mdsys=mdsys_t()
+   if len(sys.argv)==1:
+      screen_input(mdsys)
+   else:
+      file_input(mdsys)
+   allocate_arrays(mdsys)
+   read_restart(mdsys)
+   printt=prints(mdsys)
 
+
+   for i in range(mdsys.nsteps):
+      ## This is the main loop, integrator and force calculator
+      md.first_step(POINTER(mdsys))
+      md.force(mdsys)
+      md.final_step(mdsys)
+      if (mdsys.nfi%mdsys.nprint==0):
+         printt.print_output(mdsys)
 
 # fetch the data from file 
-(time, temp, Ekin, Epot, Etot) = np.loadtxt(filename, unpack = True)
+(time, temp, Ekin, Epot, Etot) = np.loadtxt(mdsys.thermo_output, unpack = True)
 ###--- the variables are loaded
-
-result = Result(time, temp, Ekin, Epot, Etot)
-
+result = Result(time,temp,Ekin,Epot,Etot)
 result.graph()
