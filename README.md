43a71d7717880d33f30b84d600ab86a21e440372
palcain <palcain@hp83-inf-9.ictp.it>
2014-03-21 03:40:19 +0100

    Documentation started. README.md file ready
    Started documentation on python files

--- README.md
+++ README.md
@@ -1,3 +1,95 @@
+CoffeeMD
+========
+
+Molecular Dynamics code in python with c bindings.
+
+This program is based on the original c code from Axel Kohlmeyer, 
+ `ljmd-c 
+<https://github.com/akohlmey/ljmd-c>`_ and refactored to
+add python bindings. It is distributed under the terms of the GNU
+General Public License.
+
+Introduction
+------------
+
+This software was made during the ICTP `Workshop on Advanced Techniques 
+for Scientific Programming and Management of Open Source Software 
+Packages <http://cdsagenda5.ictp.it/full_display.php?ida=a13190>`_, and
+its main idea is to be able to *build* molecular dynamics simulation with
+the flexibility that Python gives, leaving the hard math to c. Here, 
+building means that the c library has the minimum it should have in order
+to be able to run it with Python without losing much performance.
+
+Making CoffeeMD
+---------------
+
+To compile the dynamic loading libraries, just type 
+
+    $ make
+
+on the parent directory. This will make both the parallel and the
+serial library.
+
+Input (in Python)
+-----------------
+
+The class mdsys_t defined in python/md_classes.py directory can be
+used to input parameters in 3 different ways:
+
+- On the screen, with the method screen_input
+- Through a file, with the method file_input (see input examples *.inp
+  in the examples folder)
+- Through a GUI interface, with the method gui_input
+
+The class Potential in python/potential.py can be used to create
+different LUTs. There are predefined ones with LennardJones and
+Morse potentials, and a HomeMade potential that takes as input
+the functions for the force and the potential.
+
+Algorithm Capabilities (in c)
+-----------------------------
+
+The c code takes a LUT of the potential and integrates it with a
+typical `velocity verlet algorithm 
+<http://en.wikipedia.org/wiki/Verlet_integration>`_ in the microcanonical
+ensemble. To reproduce the canonical ensamble, an Andersen
+thermostat [Frenkel]_ is available as well.
+
+
+Output (in Python)
+------------------
+
+The output to file is managed through the method output in the
+class mdsys_t. There is also a Result class in file python/result.py
+that has the method graph, which makes plot of the energy.
+
+General Implementation (in Python)
+----------------------------------
+
+The file python/main.py is a general implementation in Python that 
+can be used as a template to make your own implementations of the code.
+
+It accepts command line arguments when running it, to trigger the 
+different input modes:
+
+- screen mode:
+
+	$ ./main.py
+
+- file mode:
+
+	$ ./main.py -f [--file] <filename.inp>
+
+- GUI mode:
+
+	$ ./main.py -g [--gui]
+
+- for help, type:
+
+	$ ./main.py -h
+	$ ./main.py --help
+
+[Frenkel] Understanding Molecular Simulation: From Algorithms to Applications
 This package contains simplified MD code with multi-threading
 parallelization for simulating atoms with a Lennard-Jones potential.
 
--- python/create_potential.py
+++ python/create_potential.py
@@ -3,11 +3,19 @@ from math import exp
 
 class Potential(object):
     def __init__(self, rcut, npoints):
+        """
+        Constructor: sets the value of the cutoff rcut and the number
+        of points in the interpolation
+        """
         self.rcut = rcut
         self.npoints = npoints
         self.create_table()
 
     def create_table(self):
+        """
+        This method creates a table, given the functions self.V_r and self.F_r
+        If the potential for r = 0 is well designed, it is inserted.
+        """
         self.r = np.linspace(self.rcut/self.npoints,self.rcut,self.npoints)
         self.V = np.zeros(self.npoints)
         self.F = np.zeros(self.npoints)
@@ -23,48 +31,85 @@ class Potential(object):
 
 
 class LennardJones(Potential):
+    """
+    This class defines a Lennard-Jones potential
+    """
     def __init__(self, rcut, npoints, sigma, epsilon):
+        """
+        Constructor: makes F_r and V_r and builds the table with
+        typical LJ parameters
+        """
         [self.V_r, self.F_r] = self.interaction(epsilon, sigma)
         Potential.__init__(self, rcut, npoints)
  
     def interaction(self, e, s):
-	v = lambda r: 4 * e * ( (s/r)**12 - (s/r)**6 )
+	"""
+        Makes F_r and V_r
+        """ 
+        v = lambda r: 4 * e * ( (s/r)**12 - (s/r)**6 )
 	f = lambda r: 4 * e * ( 12 * (s/r)**12 - 6 *(s/r)**6 ) / r
         return v, f
                                       
 
 class Morse(Potential):
+    """
+    This class defines a Morsev potential
+    """
     def __init__(self, rcut, npoints, d, a, re):
+        """
+        Constructor: makes F_r and V_r and builds the table with
+        typical Morse parameters
+        """
         [self.V_r, self.F_r] = self.interaction(d, a, re)
         Potential.__init__(self, rcut, npoints)
  
     def interaction(self, d, a, re):
+	"""
+        Makes F_r and V_r
+        """ 
 	v = lambda r: d * ( ( 1-exp(- a * (r - re) ) ) ** 2 - 1 )
 	f = lambda r: 2 * a * d * exp(a * (re - r)) * (-1 + exp(a * (re - r)))
         return v, f
 
 class HomeMade(Potential):
+    """
+    This class is for any potential created on the fly
+    """ 
     def __init__(self, rcut, npoints, V_r, F_r):
+        """
+        Constructor: the potential and force function need to be passed
+        """
         self.V_r = V_r
         self.F_r = F_r
         Potential.__init__(self, rcut, npoints)
 
 if __name__ == "__main__":
+    """
+    Just run some examples and check if everything makes sense
+    """
     import pylab as pl
+
     LJPot = LennardJones(8.5, 10000, 3.405, 0.2379)
     pl.plot(LJPot.r, LJPot.V)
-    pl.ylim(-2, 2)
+    pl.ylim(-0.5, 1)
+    pl.ylabel("Potential")
+    pl.xlabel("Distance")
     pl.show()
-    pl.plot(LJPot.r[:-1], LJPot.F[:-1]-np.diff(LJPot.V)/np.diff(LJPot.r))
-    #pl.ylim(-2, 2)
-    pl.show()
+
     MPot = Morse(5.5, 1000, 1.0, 1.0, 1.0)
     pl.plot(MPot.r, MPot.V)
     pl.ylim(-2, 2)
+    pl.ylabel("Potential")
+    pl.xlabel("Distance")
     pl.show()
+
+    #We must define our own functions if we want to use HomeMade
     V_Spring = lambda r: 0.5 * r**2
     F_Spring = lambda r: -r
+
     SPot = HomeMade(5.5, 1000, V_Spring, F_Spring)
     pl.plot(SPot.r, SPot.V)
     pl.ylim(0, 0.5*6*6)
+    pl.ylabel("Potential")
+    pl.xlabel("Distance")
     pl.show()
